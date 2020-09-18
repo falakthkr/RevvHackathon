0e309add35a57602b366566c85e599f9a57bcb11
Falak <falak@hotmail.co.in>
2020-09-18 10:18:59 +0530

    Initial Commit - RevvAssistant

---
+++ precache-manifest.4a244b45657d78a8bd10b98584a12956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1387a74b23203bd9de7c993e03e89380",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a86ab58073785e80e7",
+    "url": "/static/css/main.d57f5664.chunk.css"
+  },
+  {
+    "revision": "6e965d4c3230c5484f9b",
+    "url": "/static/js/2.eb508e0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb508e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a86ab58073785e80e7",
+    "url": "/static/js/main.770a5d36.chunk.js"
+  },
+  {
+    "revision": "b92a1c791ddf15ed48c4",
+    "url": "/static/js/runtime-main.885dff2f.js"
+  }
+]);
